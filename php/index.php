b09f6e4af6029c2ac04a0b364881a06cec0bffd1
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-24 16:11:40 +0700

    function, variable, php eol

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Ini Hello World
+echo "Hello World ", "Hello World" . PHP_EOL;
+
+$jumlah = 200;
+echo $jumlah . PHP_EOL;
+
+
+$nama = 'ukhasyah';
+echo strtoupper($nama) . PHP_EOL;
+echo ucwords($nama) . PHP_EOL;
