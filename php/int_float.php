ed2f32dead3702bbba230043ee5c00850864638c
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-24 16:23:22 +0700

    string, float & int

---
+++ php/int_float.php
@@ -0,0 +1,18 @@
+<?php
+
+// int
+$angka = 1999;
+
+// float
+$float = 3.333333;
+echo is_float($float) . PHP_EOL;
+
+// Biar dibulatkan
+echo round($float) . PHP_EOL;
+
+// increment
+// $angka--;
+$angka++;
+echo $angka;
+
+
---
+++ php/string.php
@@ -0,0 +1,5 @@
+<?php
+
+$nama = 'Ukhasyah';
+$str = "Selamat datang $nama";
+echo $str . PHP_EOL;
