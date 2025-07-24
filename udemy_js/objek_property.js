a6706321c0446baef9e0c8232e7059418d1021d8
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-24 08:14:54 +0700

    DOM pd jQuery

--- udemy_js/objek_property.js
+++ udemy_js/objek_property.js
@@ -3,5 +3,14 @@ var Mobil = {
   tipe: "Mobilio",
   wara: "Putih",
 
-  
+  maju: function () {
+    return "Mobil Maju";
+  },
+
+  mundur: () => {
+    return "Mobil Mundur";
+  },
 };
+
+console.log(Mobil.maju());
+console.log(Mobil);
